410d52689e4da9513440bdbcafd69185580f8302
wildcard-ai <70803587+wildcard-ai@users.noreply.github.com>
2023-05-08 20:59:21 +0600

    Update initialize.php

--- private/initialize.php
+++ private/initialize.php
@@ -1,9 +1,7 @@
 <?php
-  // Start output buffering
-  ob_start();
+  ob_start(); // output buffering is turned on
 
-  // Start session
-  session_start();
+  session_start(); // turn on sessions
 
   // Assign file paths to PHP constants
   // __FILE__ returns the current path to this file
@@ -32,7 +30,6 @@
 
   // Set up database connection
   $db = db_connect();
-
   $errors = [];
 
   // Set up website name

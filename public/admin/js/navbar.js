3c3b2c0eaecd3721f2b7688cda5fd498118e8650
wildcard-ai <70803587+wildcard-ai@users.noreply.github.com>
2023-05-13 22:05:13 +0600

    update navbar.js

--- public/admin/js/navbar.js
+++ public/admin/js/navbar.js
@@ -19,35 +19,40 @@ toggleButton.addEventListener('click', function() {
 function slideDown(element) {
   isAnimating = true; // Set the flag to indicate animation is in progress
   element.classList.remove('collapse');
-  var height = element.clientHeight;
+  var height = element.clientHeight; // Get the current height of the element
   element.classList.add('collapsing');
 
   setTimeout(function() {
-    element.style.height = height + 'px';
-    element.addEventListener('transitionend', function onTransitionEnd() {
-      element.removeEventListener('transitionend', onTransitionEnd);
-      element.classList.remove('collapsing');
-      element.classList.add('open');
-      toggleButton.classList.add('toggled');
-      isAnimating = false; // Reset the flag after animation completes
-    });
+    element.style.height = height + 'px'; // Animate to the computed height
   }, 0);
+
+  element.addEventListener('transitionend', function onTransitionEnd() {
+    element.removeEventListener('transitionend', onTransitionEnd);
+    element.classList.remove('collapsing');
+    element.classList.add('open');
+    toggleButton.classList.add('toggled');
+    isAnimating = false; // Reset the flag after animation completes
+    element.style.height = ''; // Remove the inline height style
+  });
 }
 
 function slideUp(element) {
   isAnimating = true; // Set the flag to indicate animation is in progress
+  var height = element.clientHeight; // Get the original height
+  element.style.height = height + 'px'; // Set the initial height
+  
   element.classList.remove('open');
   element.classList.add('collapsing');
   setTimeout(function() {
-    element.addEventListener('transitionend', function onTransitionEnd() {
-      element.removeEventListener('transitionend', onTransitionEnd);
-      element.classList.remove('collapsing');
-      element.classList.add('collapse');
-      element.style.height = '';
-      toggleButton.classList.remove('toggled');
-      isAnimating = false; // Reset the flag after animation completes
-    });
-
-    element.style.height = '0';
+    element.style.height = ''; // Animate to height 0
   }, 0);
+
+  element.addEventListener('transitionend', function onTransitionEnd() {
+    element.removeEventListener('transitionend', onTransitionEnd);
+    element.classList.remove('collapsing');
+    element.classList.add('collapse');
+    toggleButton.classList.remove('toggled');
+    isAnimating = false; // Reset the flag after animation completes
+  });
 }
+
